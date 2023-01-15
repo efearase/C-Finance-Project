161c8ee92aa4104e09f6a0cf5263f12bdae9745b
efearase <75952778+efearase@users.noreply.github.com>
2023-01-15 19:51:20 +0000

    commit message

--- .ccls-cache/@home@runner@CPP/Option.h
+++ .ccls-cache/@home@runner@CPP/Option.h
@@ -12,7 +12,6 @@ class Option // This class members correspond to options object.
         PayOff* pay_off;
         Option(std::map<std::string, std::string> parameters);
         double strike_price, volatility, spot_price, interest_rate, nb_of_years_before_maturity;
-  
 };
 
 #endif
